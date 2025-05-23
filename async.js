0cb4ad65e23e9582a9068ffc7f7e2a07dbd0d446
ShahanArshad86 <118489074+ShahanArshad86@users.noreply.github.com>
2025-05-23 05:25:23 +0500

    master
    new changes to async.js

--- async.js
+++ async.js
@@ -1,12 +1,14 @@
-const fetchData = () => {
+const fetchData = callback => {
   setTimeout(() => {
-cancelIdleCallback()
-
-  }, 1500);
+    callback('Done!');
+    }, 1500);
 };
 
 setTimeout(() => {
   console.log("Timeout is implemented");
+  fetchData(text => {
+    console.log(text);
+  });
 }, 7000); //delya of 7 sec is added, example of async code block
 
 console.log("Hi, this is Shahan"); //sync code examples, as they do not have any delay.
