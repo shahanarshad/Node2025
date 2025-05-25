153f50852d45f2e1ba068d10137c83c08fdec80c
ShahanArshad86 <118489074+ShahanArshad86@users.noreply.github.com>
2025-05-26 04:45:45 +0500

    Master:
    last commit of the lecture, added template literal example and promises code.

--- async.js
+++ async.js
@@ -1,18 +1,23 @@
 const fetchData = () => {
-const promise= new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Done!');
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
     }, 1500);
   });
-return promise;
+  return promise;
 };
 
 
-setTimeout(() => {
+setTimeout(() => {  //main bracket
   console.log("Timeout is implemented");
-  fetchData(text => {
+  fetchData().then(text => {
     console.log(text);
+    return fetchData().then(text2 => {
+      console.log(text2);
+
+    }); //inner bracket 
   });
+
 }, 7000); //delya of 7 sec is added, example of async code block
 
 console.log("Hi, this is Shahan"); //sync code examples, as they do not have any delay.
---
+++ template_literalls.js
@@ -0,0 +1,6 @@
+const fetchData=((num1, num2)=>{
+console.log(`This is his real age ${num1} and his name is ${num2}`);
+});
+
+fetchData(2,5);
+
